88207bfef73511eb8b831a91f79eaf0e16b74d0d
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-01 19:49:19 -0300

    modifico el archivo app.js y package.json para ver los cambios de la paginas en tiempo real

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+
+const connectLivereload = require("connect-livereload");
+
+
 const express = require('express');
 
 const path = require('path');
@@ -10,6 +16,18 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
+liveReloadServer.watch(path.join(__dirname,'public'));
+
+app.use(connectLivereload());
+
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')));
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
 
 app.listen(port,() => console.log(`El servidor fue levantado correctamente en http://localhost:${port}`));
